ee776855ea5a191bd8f24f250ce0166bd748b415
Denis Wilton <35562940+DennisWilton@users.noreply.github.com>
2023-03-19 15:33:41 -0400

    Added header and news at dashboard

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,1 +1,173 @@
-<template>This is the dashboard</template>
+<template>
+  <div class="dashboard">
+    <div class="dashboard__container">
+      <div class="dashboard__outbox">
+        <div class="dashboard__news">
+          <div class="dashboard__news__time">11:48</div>
+          <div class="dashboard__news__title">
+            BTC atinge marca histórica: entenda o impacto na economia global
+          </div>
+          <div class="dashboard__news__author">Binance Brasil</div>
+        </div>
+        <div class="dashboard__outbox__settings">
+          <GearIcon />
+        </div>
+      </div>
+
+      <div class="dashboard__content">
+        <div class="dashboard__content__header">
+          <div class="dashboard__content__profile">
+            <DashboardProfile />
+          </div>
+          <div class="dashboard__content__settings">
+            <button>Configurações</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DashboardProfile from './DashboardProfile.vue';
+import GearIcon from '../../icons/Gear.vue';
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  min-height: 100%;
+  background: #eeeeee;
+  display: grid;
+  place-items: center;
+  padding: 60px 0;
+
+  &__container {
+    background: #fafafa;
+    width: 100%;
+    height: 100%;
+    max-width: 1088px;
+    display: grid;
+    grid-template-rows: 20px 1fr;
+  }
+
+  &__outbox {
+    transform: translateY(-50%);
+    height: min-content;
+    display: grid;
+    grid-template-columns: auto auto;
+    padding: 0 1.5rem;
+    grid-gap: 1rem;
+    align-items: center;
+    justify-items: center;
+    height: 40px;
+
+    &__settings {
+      display: grid;
+      place-items: center;
+    }
+  }
+
+  &__news {
+    background: #222;
+    color: white;
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    height: 100%;
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    grid-template-columns: auto 1fr auto;
+    gap: 10px;
+    padding: 0px 20px;
+    border-radius: 20px;
+
+    &__time {
+      font-weight: bold;
+    }
+
+    &__title {
+      display: block;
+      width: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    &__author {
+      font-weight: bold;
+    }
+  }
+
+  &__content {
+    padding: 1rem 2rem;
+
+    &__header {
+      color: #000;
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    &__settings {
+      display: none;
+
+      & > button {
+        margin: 0;
+        border: none;
+        color: #000;
+        padding: 10px;
+        display: flex;
+        background: transparent;
+        align-items: center;
+        line-height: 100%;
+        border-radius: 10px;
+        cursor: pointer;
+        user-select: none;
+
+        &:hover {
+          background: #fff;
+        }
+
+        &:active {
+          background: #efefef;
+        }
+
+        /* hide outline */
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+  }
+}
+
+@media (min-width: 768px) {
+  .dashboard {
+    padding: 60px;
+    &__outbox {
+      grid-template-columns: 1fr;
+
+      &__settings {
+        display: none;
+      }
+    }
+
+    &__container {
+      border-radius: 20px;
+    }
+
+    &__content {
+      &__header {
+        grid-template-columns: 1fr 1fr;
+      }
+      &__settings {
+        display: flex;
+        align-items: center;
+        justify-content: end;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Dashboard/DashboardProfile.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="dashboard__profile">
+    <div class="dashboard__profile__avatar">
+      <img
+        src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+        alt="avatar"
+      />
+    </div>
+    <div class="dashboard__profile__info">
+      <h2 class="dashboard__profile__info__name">Fernando Dias</h2>
+      <p>Membro Diamante PLUS</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.dashboard__profile {
+  color: #000;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 1rem;
+  align-items: center;
+  justify-content: space-between;
+
+  &__avatar {
+    width: min-content;
+    img {
+      width: 89px;
+      height: 89px;
+      border-radius: 50%;
+      pointer-events: none;
+    }
+  }
+
+  &__info {
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    gap: 0.25rem;
+
+    h2 {
+      font-size: 22px;
+      line-height: 100%;
+    }
+    p {
+      font-weight: 300;
+      font-size: 17px;
+      color: #6b6b6b;
+      line-height: 100%;
+    }
+  }
+}
+</style>
