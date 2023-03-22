9b0daaa76276ab5701f24f64012a089bd5689563
Denis Wilton <35562940+DennisWilton@users.noreply.github.com>
2023-03-22 02:51:11 -0400

    Fix

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,18 +1,20 @@
 <template>
-  <h1>
-    {{
-      AuthManager.isLogged
-        ? `You are in, ${AuthManager.user?.username}`
-        : `Sorry! You are out.`
-    }}
-  </h1>
-  <div>
-    <span>{{ AuthManager.user.username }}</span>
-  </div>
-  <div>
+  <div class="login">
+    <h1>Você está deslogado.</h1>
     <button @click="AuthManager.login">Logar</button>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
 
 <script setup lang="ts">
 import { watch } from 'vue';
